7ba6f66be23ad8451fd323a5a5f714d51c42a3aa
GrassTypeProgrammer <lukeanthonyreeve@gmail.com>
2024-11-13 19:26:59 +0000

    added ability to alter task name. Also added filter buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+  };
+
 function App(props) {
+    
     const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState("All");
+    const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+    const filterList = FILTER_NAMES.map((name) => (
+        <FilterButton 
+            key={name} 
+            name={name} 
+            isPressed={name === filter}
+            setFilter={setFilter}
+        />
+      ));
 
     return (
       <div className="todoapp stack-large">
         <h1>TODO</h1>
         <Form onSubmit={addTask}/>
         <div className="filters btn-group stack-exception">
-            <FilterButton label="All"/>
-            <FilterButton label="Active"/>
-            <FilterButton label="Completed"/>
-          
+           {filterList}
         </div>
         <h2 id="list-heading">3 tasks remaining</h2>
         <ul
@@ -40,6 +55,7 @@ function App(props) {
             key={task.id}
             toggleTaskCompleted={toggleTaskCompleted}
             onDeleteTask={deleteTask}
+            editTask={editTask}
         />)
       }
   
@@ -58,7 +74,6 @@ function App(props) {
         setTasks(remainingTasks);
     }
 
-
     function toggleTaskCompleted(id){
         const updatedTasks = tasks.map((task) => {
             if (id === task.id) {
@@ -69,6 +84,18 @@ function App(props) {
         });
 
         setTasks(updatedTasks);
+    }
+
+    function editTask(id, newName) {
+        const editedTasks = tasks.map((task) => {
+            if (id === task.id) {
+                return { ...task, name: newName };
+            }
+            
+            return task;
+        })
+
+        setTasks(editedTasks);
     }
 }
   
